ed49216e7ea5dc4acfe2276f925c7c07dcd97cfa
enkod-id <natanaeltamto@gmail.com>
2024-02-12 09:20:26 +0700

    update

---
+++ resources/views/partials/head.blade.php
@@ -0,0 +1,20 @@
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <title>SATUDIGITAL</title>
+    <meta content="ONE STOP SOLUTION" name="description" />
+    <meta content="Themesdesign" name="author" />
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+    <link href="../plugins/bootstrap-datepicker/css/bootstrap-datepicker.min.css" rel="stylesheet">
+
+    <!--Morris Chart CSS -->
+    <link rel="stylesheet" href="../plugins/morris/morris.css">
+
+    <link href="{{asset('')}}assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="{{asset('')}}assets/css/metismenu.min.css" rel="stylesheet" type="text/css">
+    <link href="{{asset('')}}assets/css/icons.css" rel="stylesheet" type="text/css">
+    <link href="{{asset('')}}assets/css/style.css" rel="stylesheet" type="text/css">
+
+</head>
